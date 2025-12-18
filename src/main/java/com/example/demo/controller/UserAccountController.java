b669ca634ae7b61b0652a045d7d59e2ee1bcc1a5
bala-amypo <bala@amypo.in>
2025-12-18 07:48:08 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -0,0 +1,35 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.UserAccount;
+import com.example.demo.service.UserAccountService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/auth")
+@Tag(name = "Authentication")
+public class UserAccountController {
+
+    private final UserAccountService service;
+
+    public UserAccountController(UserAccountService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/register")
+    public UserAccount register(@RequestBody UserAccount user) {
+        return service.register(user);
+    }
+
+    @GetMapping("/users")
+    public List<UserAccount> getAllUsers() {
+        return service.getAllUsers();
+    }
+
+    @GetMapping("/users/{id}")
+    public UserAccount getUser(@PathVariable Long id) {
+        return service.getUser(id);
+    }
+}
