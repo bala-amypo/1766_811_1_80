2ddf166cd1a91a266ce17201df2ad2e23f251f79
bala-amypo <bala@amypo.in>
2025-12-19 14:48:50 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -35,4 +35,64 @@ public class ClashRecord {
     }
 
     // Getters & Setters
+    public Long getId() {
+    return id;
+}
+
+public void setId(Long id) {
+    this.id = id;
+}
+
+public Long getEventAId() {
+    return eventAId;
+}
+
+public void setEventAId(Long eventAId) {
+    this.eventAId = eventAId;
+}
+
+public Long getEventBId() {
+    return eventBId;
+}
+
+public void setEventBId(Long eventBId) {
+    this.eventBId = eventBId;
+}
+
+public String getClashType() {
+    return clashType;
+}
+
+public void setClashType(String clashType) {
+    this.clashType = clashType;
+}
+
+public String getSeverity() {
+    return severity;
+}
+
+public void setSeverity(String severity) {
+    this.severity = severity;
+}
+
+public String getDetails() {
+    return details;
+}
+
+public void setDetails(String details) {
+    this.details = details;
+}
+
+public Boolean getResolved() {
+    return resolved;
+}
+
+public void setResolved(Boolean resolved) {
+    this.resolved = resolved;
+}
+
+public LocalDateTime getDetectedAt() {
+    return detectedAt;
+}
+
 }
