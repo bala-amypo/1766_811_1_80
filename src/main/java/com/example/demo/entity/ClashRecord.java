18dde8b912dcabed7dd22d6337d94c168a1085de
bala-amypo <bala@amypo.in>
2025-12-22 10:09:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/ClashRecord.java
+++ src/main/java/com/example/demo/entity/ClashRecord.java
@@ -18,7 +18,7 @@ public class ClashRecord {
 
     private String description;
 
-    // Getters and Setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
