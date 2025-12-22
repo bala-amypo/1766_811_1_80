7124d5cd979717c1cd773ac0df3f9478cc2cb850
bala-amypo <bala@amypo.in>
2025-12-22 08:48:24 +0000

    User commit

--- src/main/java/com/example/demo/controller/AcademicEventController.java
+++ src/main/java/com/example/demo/controller/AcademicEventController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.AcademicEvent;
 import com.example.demo.service.AcademicEventService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/events")
+@Tag(name = "Academic Events")
 public class AcademicEventController {
 
     private final AcademicEventService service;
@@ -22,8 +24,25 @@ public class AcademicEventController {
         return service.save(event);
     }
 
+    @PutMapping("/{id}")
+    public AcademicEvent update(
+            @PathVariable Long id,
+            @Valid @RequestBody AcademicEvent event) {
+        return service.update(id, event);
+    }
+
+    @GetMapping("/{id}")
+    public AcademicEvent getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+
     @GetMapping
     public List<AcademicEvent> getAll() {
         return service.getAll();
+    }
+
+    @GetMapping("/branch/{branchId}")
+    public List<AcademicEvent> getByBranch(@PathVariable Long branchId) {
+        return service.getByBranch(branchId);
     }
 }
