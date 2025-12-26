1a6daa5f8328b1dca4a2b43b5d743f8635e38794
bala-amypo <bala@amypo.in>
2025-12-26 17:06:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/AcademicEventRepository.java
+++ src/main/java/com/example/demo/repository/AcademicEventRepository.java
@@ -1,4 +1,4 @@
-package com.example.demo.repository;
+/*package com.example.demo.repository;
 
 import com.example.demo.entity.AcademicEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,3 +9,15 @@ public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Lo
 
     List<AcademicEvent> findByBranchId(Long branchId);
 }
+*/
+package com.example.demo.repository;
+
+import com.example.demo.entity.AcademicEvent;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AcademicEventRepository extends JpaRepository<AcademicEvent, Long> {
+    List<AcademicEvent> findByBranchId(Long branchId);
+}
+
