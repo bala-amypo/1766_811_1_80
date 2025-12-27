998d8a55f8f61ef82c3ebcad67480f7d20498ff3
bala-amypo <bala@amypo.in>
2025-12-27 01:57:41 +0000

    User commit

--- src/main/java/com/example/demo/dto/MergeEventsRequest.java
+++ src/main/java/com/example/demo/dto/MergeEventsRequest.java
@@ -1,4 +1,4 @@
-package com.example.demo.dto;
+/*package com.example.demo.dto;
 
 import java.util.List;
 
@@ -21,5 +21,37 @@ public class MergeEventsRequest {
 
     public String getMergeReason() {
         return mergeReason;
+    }
+}
+*/
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class MergeEventsRequest {
+    private List<Long> eventIds;
+    private String mergeReason;
+
+    public MergeEventsRequest() {}
+
+    public MergeEventsRequest(List<Long> eventIds, String mergeReason) {
+        this.eventIds = eventIds;
+        this.mergeReason = mergeReason;
+    }
+
+    public List<Long> getEventIds() {
+        return eventIds;
+    }
+
+    public void setEventIds(List<Long> eventIds) {
+        this.eventIds = eventIds;
+    }
+
+    public String getMergeReason() {
+        return mergeReason;
+    }
+
+    public void setMergeReason(String mergeReason) {
+        this.mergeReason = mergeReason;
     }
 }
